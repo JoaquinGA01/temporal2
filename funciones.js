97e9625e618775aa3d5f1bff26ce2f2428f3b0de
JoaquinGA01 <otrebla.2817@gmail.com>
2021-10-06 11:17:34 -0500

    CAmbios

---
+++ funciones.js
@@ -0,0 +1,18 @@
+function setResult(value) {
+    document.calc.result.value = value;
+}
+function getResult() {
+    return(document.calc.result.value);
+}
+function add(key) { 
+    var result = getResult();
+    if (result!='0' || isNaN(key)) setResult(result + key);
+    else setResult(key);
+}
+function calc() {
+    var result = eval(getResult()); 
+    setResult("document.calc.resultado.value=eval(document.calc.resultado.value)");
+}
+function del() { 
+    setResult(0);
+}
