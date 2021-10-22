4339daab2cd7514577eb78ce69c59a68bc2c0d5a
JoaquinGA01 <otrebla.2817@gmail.com>
2021-10-22 14:09:29 -0500

    Version Final Login

--- thymeleaf/thymeleaf/src/main/java/mx/uv/demo/App.java
+++ thymeleaf/thymeleaf/src/main/java/mx/uv/demo/App.java
@@ -22,7 +22,7 @@ public class App
     {
         List<Usuarios> usuarios = new ArrayList<Usuarios>();
         port(8080);
-
+        staticFiles.location("/");
         get("/",(rq, rs)->{
             usuarios.add(new Usuarios(1L,"Pedro","AInEk1020"));
             usuarios.add(new Usuarios(1L,"Joaquin","Kenia25"));
@@ -31,13 +31,13 @@ public class App
             Map<String, Object> variables = new HashMap<>();
             variables.put("today", new Date());
             IContext context = new Context(rq.raw().getLocale(), variables);
-            String out = ThymeleafUtil.getTemplateEngine(null).process("home", context);
+            String out = ThymeleafUtil.getTemplateEngine().process("home", context);
             return out;
         });
 
         get("/hola", (req,rs)->{
             
-            rs.redirect("/resources/Login.html");
+            rs.redirect("/Login.html");
             return null;
             /*Map<String, Object> variables = new HashMap<>();
             variables.put("Lista", usuarios);
@@ -61,19 +61,19 @@ public class App
                         variables.put("User", user);
                         variables.put("Lista", usuarios);                        
                         IContext context = new Context(rq.raw().getLocale(), variables);
-                        String out = ThymeleafUtil.getTemplateEngine("Hola").process("Tabla", context);
+                        String out = ThymeleafUtil.getTemplateEngine().process("Tabla", context);
                         return out;
                     }else{
                         variables.put("Error", "Contraseña Diferente");
                         IContext context = new Context(rq.raw().getLocale(), variables);
-                        String out = ThymeleafUtil.getTemplateEngine("Hola").process("Errores", context);
+                        String out = ThymeleafUtil.getTemplateEngine().process("Errores", context);
                         return out;
                     }
                 }
             }
             variables.put("Error", "Usuario Inexistente");
             IContext context = new Context(rq.raw().getLocale(), variables);
-            String out = ThymeleafUtil.getTemplateEngine("Hola").process("Errores", context);
+            String out = ThymeleafUtil.getTemplateEngine().process("Errores", context);
             return out;    
         });
 
@@ -85,14 +85,14 @@ public class App
             if(user.equals("")){
                 variables.put("Error", "Agrega un Nombre de Usuario");
                 IContext context = new Context(rq.raw().getLocale(), variables);
-                String out = ThymeleafUtil.getTemplateEngine("Hola").process("Errores", context);
+                String out = ThymeleafUtil.getTemplateEngine().process("Errores", context);
                 return out;
             }
             for(Usuarios usu : usuarios){
                 if(user.equals(usu.getNombre())){
                     variables.put("Error", "Este Usuario ya Existe");
                     IContext context = new Context(rq.raw().getLocale(), variables);
-                    String out = ThymeleafUtil.getTemplateEngine("Hola").process("Errores", context);
+                    String out = ThymeleafUtil.getTemplateEngine().process("Errores", context);
                     return out;
                 }
             }
@@ -100,7 +100,7 @@ public class App
                 
                 variables.put("Error", "Rellena Todos los Campos");
                 IContext context = new Context(rq.raw().getLocale(), variables);
-                String out = ThymeleafUtil.getTemplateEngine("Hola").process("Errores", context);
+                String out = ThymeleafUtil.getTemplateEngine().process("Errores", context);
                 return out;
             }
             if(pass.equals(pass2)){
@@ -108,12 +108,12 @@ public class App
                 variables.put("Nombre", user);
                 variables.put("Lista", usuarios);                        
                 IContext context = new Context(rq.raw().getLocale(), variables);
-                String out = ThymeleafUtil.getTemplateEngine("Hola").process("Tabla", context);
+                String out = ThymeleafUtil.getTemplateEngine().process("Tabla", context);
                 return out;
             }else{
                 variables.put("Error", "Contraseña Diferente");
                 IContext context = new Context(rq.raw().getLocale(), variables);
-                String out = ThymeleafUtil.getTemplateEngine("Hola").process("Errores", context);
+                String out = ThymeleafUtil.getTemplateEngine().process("Errores", context);
                 return out;
             }
         });
--- thymeleaf/thymeleaf/src/main/java/mx/uv/demo/ThymeleafUtil.java
+++ thymeleaf/thymeleaf/src/main/java/mx/uv/demo/ThymeleafUtil.java
@@ -1,4 +1,5 @@
 package mx.uv.demo;
+
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.IContext;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -7,38 +8,30 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 public class ThymeleafUtil {
     private static TemplateEngine templateEngine;
 
-    public static void init(String ruta) {
+    public static void init() {
 
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setTemplateMode(TemplateMode.HTML);
-        if(ruta==null){
-            templateResolver.setPrefix("templates/");
-        }else{
-            templateResolver.setPrefix("resources/");
-        }
+        templateResolver.setPrefix("templates/");
+
         templateResolver.setSuffix(".html");
         templateResolver.setCacheable(false);
-        
+
         templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
     }
 
-    public static TemplateEngine getTemplateEngine(String ruta) {
-        if(ruta == null){       
-            if(templateEngine == null) { init(ruta); }    
-            return templateEngine;
-        }else{
-            if(templateEngine == null) { init(ruta); }    
-            return templateEngine;
+    public static TemplateEngine getTemplateEngine() {
+
+        if (templateEngine == null) {
+            init();
         }
+        return templateEngine;
     }
-    
-    public static String process(String template, IContext context, String ruta) {
-        if(ruta == null){
-        return getTemplateEngine(ruta).process(template, context);
-        }else{
-            return getTemplateEngine(ruta).process(template, context);
-        }
+
+    public static String process(String template, IContext context) {
+
+        return getTemplateEngine().process(template, context);
 
     }
 }
